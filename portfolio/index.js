f92b8cefc90c5939362bcadb258d356dd713af0f
k98940 <pv1xxb@yandex.ru>
2022-01-16 21:09:12 +0600

    feat: adaptive blocks 'Menu' and 'Hero' for Tablet - 768px

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,30 @@
-document.body.addEventListener('dragstart', (event) => { // предотвратить событие "перетаскивание" иконок и меню
+'use strict'
+
+const hamburger = document.querySelector('#hamburger')
+const headerNavWrapper = document.querySelector('#header-nav-wrapper')
+const headerNav = document.querySelector('#header-nav')
+
+hamburger.addEventListener('click', () => {
+   hamburger.classList.toggle('is-active')
+   headerNavWrapper.classList.toggle('nav-wrapper--active')
+   headerNav.classList.toggle('header-nav--active')
+
+   if (hamburger.classList.contains('is-active')) { // заблокировать прокрутку / свайп экрана при открытом меню
+      document.body.style.position = 'fixed'
+   } else {
+      document.body.style.position = '';
+   }
+})
+
+headerNavWrapper.addEventListener('click', (element => {
+   if (element.target.classList.contains('nav-link')) {
+      hamburger.classList.remove('is-active')
+      headerNav.classList.remove('header-nav--active')
+      headerNavWrapper.classList.remove('nav-wrapper--active')
+      document.body.style.position = '';
+   }
+}))
+
+document.body.addEventListener('dragstart', (event) => { // заблокировать "перетаскивание" иконок и картинок
    event.preventDefault()
 })
