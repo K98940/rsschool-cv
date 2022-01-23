f0a5309943000657aa0f1d2336e05fa3fd6f0504
k98940 <pv1xxb@yandex.ru>
2022-01-23 19:08:40 +0600

    feat: add zoom for pictures of block 'Portfolio', add toggle for buttons 'winter/spring/summer'

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,13 +3,19 @@
 const hamburger = document.querySelector('#hamburger')
 const headerNavWrapper = document.querySelector('#header-nav-wrapper')
 const headerNav = document.querySelector('#header-nav')
+const portfolioButtons = document.querySelector('.portfolio-btn-wrapper')
+const portfolioImages = document.querySelectorAll('.portfolio-img img')
+const imgZoomed = document.querySelector('.portfolio-img__zoomed')
 
+
+// гамбургер-меню
 hamburger.addEventListener('click', () => {
    hamburger.classList.toggle('is-active')
    headerNavWrapper.classList.toggle('nav-wrapper--active')
    headerNav.classList.toggle('header-nav--active')
 
-   if (hamburger.classList.contains('is-active')) { // заблокировать прокрутку / свайп экрана при открытом меню
+   // заблокировать прокрутку / свайп экрана при открытом меню
+   if (hamburger.classList.contains('is-active')) {
       document.body.style.position = 'fixed'
    } else {
       document.body.style.position = '';
@@ -25,6 +31,51 @@ headerNavWrapper.addEventListener('click', (element => {
    }
 }))
 
-document.body.addEventListener('dragstart', (event) => { // заблокировать "перетаскивание" иконок и картинок
+// заблокировать "перетаскивание" иконок и картинок
+document.body.addEventListener('dragstart', (event) => {
    event.preventDefault()
+})
+
+// обработка нажатий кнопок секции 'Portfolio'
+portfolioButtons.addEventListener('click', (event) => {
+   if (event.target.classList.contains('btn__portfolio')) {
+      portfolioButtons.querySelectorAll('button').forEach(btn => {
+         btn.classList.remove('btn__active')
+      })
+      if (event.target.innerHTML === 'Winter') {
+         event.target.classList.add('btn__active')
+         portfolioImages.forEach((img, index) => {
+            img.src = `./assets/img/portfolio/winter/${index}.jpg`
+         })
+      }
+      if (event.target.innerHTML === 'Spring') {
+         event.target.classList.add('btn__active')
+         portfolioImages.forEach((img, index) => {
+            img.src = `./assets/img/portfolio/spring/${index}.jpg`
+         })
+      }
+      if (event.target.innerHTML === 'Summer') {
+         event.target.classList.add('btn__active')
+         portfolioImages.forEach((img, index) => {
+            img.src = `./assets/img/portfolio/summer/${index}.jpg`
+         })
+      }
+   }
+})
+
+// обработка нажатий на фотографии секции 'Portfolio'
+document.addEventListener('click', event => {
+   console.log(event.target)
+   console.log(event.target.closest('.portfolio-img'))
+   console.log(event.clientX, event.clientY)
+
+   if (imgZoomed.classList.contains('portfolio-img__zoomed--active')) { //свернуть zoom
+      document.body.style.overflow = 'auto'
+      imgZoomed.classList.remove('portfolio-img__zoomed--active')
+   }
+   if (event.target.closest('.portfolio-img')) { // развернуть zoom на весь экран
+      document.body.style.overflow = 'hidden'
+      imgZoomed.classList.add('portfolio-img__zoomed--active')
+      imgZoomed.querySelector('img').src = event.target.src
+   }
 })
