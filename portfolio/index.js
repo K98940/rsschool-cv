e7c4529e35450cb20c4d7134c0cdeb8031e410b7
k98940 <pv1xxb@yandex.ru>
2022-02-05 21:26:39 +0600

    feat: add custom video player

--- portfolio/index.js
+++ portfolio/index.js
@@ -2,6 +2,7 @@
 
 let theme = 'dark'
 let language = 'en'
+let curTime = 0
 
 const hamburger = document.querySelector('#hamburger')
 const headerNavWrapper = document.querySelector('#header-nav-wrapper')
@@ -12,35 +13,18 @@ const imgZoomed = document.querySelector('.portfolio-img__zoomed')
 const swTheme = document.querySelector('#swTheme') // кнопка переключения темы светлая / темная
 const svg = document.querySelectorAll('#swTheme svg')
 const switchs = document.querySelector('.switchs')
+// video player
+const videoWrapper = document.querySelector('.video-wrapper')
+const player = document.querySelector('.player')
+const playerButton = document.querySelector('.player-button')
+const playerTime = document.querySelector('.player-time span')
+const progressFill = document.querySelector('.player-progress-fill')
+const playerProgress = document.querySelector('.player-progress')
+const progressSpan = document.querySelector('.progress-span')
+const playerVolume = document.querySelector('.player-volume')
+const playerMute = document.querySelector('.player-mute')
 
 
-// гамбургер-меню
-hamburger.addEventListener('click', () => {
-   hamburger.classList.toggle('is-active')
-   headerNavWrapper.classList.toggle('nav-wrapper--active')
-   headerNav.classList.toggle('header-nav--active')
-
-   // заблокировать прокрутку / свайп экрана при открытом меню
-   if (hamburger.classList.contains('is-active')) {
-      document.body.style.position = 'fixed'
-   } else {
-      document.body.style.position = '';
-   }
-})
-
-headerNavWrapper.addEventListener('click', (element => {
-   if (element.target.classList.contains('nav-link')) {
-      hamburger.classList.remove('is-active')
-      headerNav.classList.remove('header-nav--active')
-      headerNavWrapper.classList.remove('nav-wrapper--active')
-      document.body.style.position = '';
-   }
-}))
-
-// заблокировать "перетаскивание" иконок и картинок
-document.body.addEventListener('dragstart', (event) => {
-   event.preventDefault()
-})
 
 // обработка нажатий кнопок секции 'Portfolio'
 portfolioButtons.addEventListener('click', (event) => {
@@ -54,6 +38,8 @@ portfolioButtons.addEventListener('click', (event) => {
       })
    }
 })
+
+
 
 // обработка нажатий на фотографии секции 'Portfolio'
 document.addEventListener('click', event => {
@@ -234,12 +220,16 @@ function getTranslate(lang) {
    }
 }
 
+
+
 // перевод страницы
 switchs.addEventListener('click', () => {
    language = language === 'en' ? 'ru' : 'en'
    localStorage.setItem('language', language) // сохранить состояние
    getTranslate(language)
 })
+
+
 
 //загрузка страницы, восстановление сохраненных состояний
 window.addEventListener('load', () => {
@@ -252,4 +242,154 @@ window.addEventListener('load', () => {
       theme = localStorage.getItem('theme')
       setTheme()
    }
+
+   if (localStorage.getItem('curTime')) {
+      curTime = localStorage.getItem('curTime')
+      player.currentTime = curTime
+      progressFill.style.width = `${curTime / localStorage.getItem('videoDuration') * 100}%`
+   }
+
+   if (localStorage.getItem('volume')) {
+      player.volume = localStorage.getItem('volume')
+      playerVolume.value = player.volume
+      playerVolume.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${playerVolume.value * 100}%, #fff ${playerVolume.value * 100}%, white 100%)`
+   }
+
+   if (localStorage.getItem('mute') === 'true') {
+      if (localStorage.getItem('mute')) {
+         player.volume = 0
+         playerMute.classList.add('mute')
+      }
+   }
+
+})
+
+
+// video player
+function secondsToTime(time) {
+   let h = Math.floor(time / (60 * 60))
+   let dm = time % (60 * 60)
+   let m = Math.floor(dm / 60)
+   let ds = dm % 60
+   let s = Math.ceil(ds)
+   let fulltime = ''
+
+   if (s === 60) {
+      s = 0
+      m = m + 1
+   }
+   if (s < 10) {
+      s = '0' + s
+   }
+   if (m === 60) {
+      m = 0
+      h = h + 1
+   }
+   if (m < 10) {
+      m = '0' + m
+   }
+   if (h === 0) {
+      fulltime = m + ':' + s
+   } else {
+      fulltime = h + ':' + m + ':' + s
+   }
+   return fulltime;
+}
+
+
+
+function updateButton() {
+   const icon = this.paused ? '\u25B6' : '\u2016'
+   playerButton.textContent = icon
+}
+
+
+
+function togglePlay() {
+   const method = player.paused ? 'play' : 'pause'
+   player[method]()
+}
+
+
+
+function updateProgress() {
+   let t = Math.trunc(player.currentTime)
+
+   if ((t % 10 === 0) && (curTime != t)) { // сохранить состояние каждые 10 сек
+      localStorage.setItem('curTime', t)
+      localStorage.setItem('videoDuration', player.duration)
+      curTime = t
+   }
+
+   playerTime.textContent = `${secondsToTime(t)} : ${secondsToTime(player.duration)}` // показать текущее время воспроизведения
+   progressFill.style.width = `${t / player.duration * 100}%`
+}
+
+
+
+function scrub(event) {
+   const scrubTime = (event.offsetX / playerProgress.offsetWidth) * player.duration
+   player.currentTime = scrubTime
+}
+
+
+function showTime(event) {
+   const scrubTime = (event.offsetX / playerProgress.offsetWidth) * player.duration
+   progressSpan.textContent = secondsToTime(scrubTime)
+}
+
+
+function changeVolume() {
+   player.volume = playerVolume.value
+   console.log(playerVolume.value)
+   localStorage.setItem('volume', player.volume)
+   playerVolume.style.background = `linear-gradient(to right, #bdae82 0%, #bdae82 ${playerVolume.value * 100}%, #fff ${playerVolume.value * 100}%, white 100%)`
+   playerMute.classList.remove('mute')
+   localStorage.setItem('mute', playerMute.classList.contains('mute'))
+}
+
+
+function muteVolume() {
+   player.volume = (player.volume > 0) ? 0 : playerVolume.value
+   playerMute.classList.toggle('mute')
+   localStorage.setItem('mute', playerMute.classList.contains('mute'))
+}
+
+
+player.addEventListener('click', togglePlay)
+playerButton.addEventListener('click', togglePlay)
+player.addEventListener('play', updateButton)
+player.addEventListener('pause', updateButton)
+player.addEventListener('timeupdate', updateProgress)
+playerProgress.addEventListener('click', scrub)
+playerProgress.addEventListener('mousemove', showTime)
+playerVolume.addEventListener('input', changeVolume)
+playerMute.addEventListener('click', muteVolume)
+
+// гамбургер-меню
+hamburger.addEventListener('click', () => {
+   hamburger.classList.toggle('is-active')
+   headerNavWrapper.classList.toggle('nav-wrapper--active')
+   headerNav.classList.toggle('header-nav--active')
+
+   // заблокировать прокрутку / свайп экрана при открытом меню
+   if (hamburger.classList.contains('is-active')) {
+      document.body.style.position = 'fixed'
+   } else {
+      document.body.style.position = '';
+   }
+})
+
+headerNavWrapper.addEventListener('click', (element => {
+   if (element.target.classList.contains('nav-link')) {
+      hamburger.classList.remove('is-active')
+      headerNav.classList.remove('header-nav--active')
+      headerNavWrapper.classList.remove('nav-wrapper--active')
+      document.body.style.position = '';
+   }
+}))
+
+// заблокировать "перетаскивание" иконок и картинок
+document.body.addEventListener('dragstart', (event) => {
+   event.preventDefault()
 })
